47404f4f364c563d54b6b31d67fd0fd47522342b
Dave Chen <dchen@mail.nih.gov>
2020-07-02 12:12:54 -0400

    flake8 fixes

--- drt2volume.py
+++ drt2volume.py
@@ -1,14 +1,11 @@
 
 import math
-import pydicom
-import SimpleITK as sitk
+import vtk
+import drt2polylines
 
-import utils
-from drt2polylines import *
 
-def contourSequence2Volume(cs, spacing=[1,1,1]):
-
-    polylines = contourSequence2PolyLines(cs)
+def contourSequence2Volume(cs, spacing=[1, 1, 1]):
+    polylines = drt2polylines.contourSequence2PolyLines(cs)
 
     stencil = vtk.vtkPolyDataToImageStencil()
     stencil.SetInputData(polylines)
@@ -18,14 +15,14 @@ def contourSequence2Volume(cs, spacing=[1,1,1]):
     bounds2 = []
     for x in bounds:
         if i & 1:
-            y = math.ceil(x)+1
+            y = math.ceil(x) + 1
         else:
-            y = math.floor(x)-1
+            y = math.floor(x) - 1
         bounds2.append(y)
-        i=i+1
+        i = i + 1
 
-    stencil.SetOutputWholeExtent( bounds2[0], bounds2[1], bounds2[2],
-        bounds2[3], bounds2[4], bounds2[5] )
+    stencil.SetOutputWholeExtent(bounds2[0], bounds2[1], bounds2[2],
+                                 bounds2[3], bounds2[4], bounds2[5])
 
     stencil.SetOutputSpacing(spacing[0], spacing[1], spacing[2])
     stencil.Update()
@@ -35,5 +32,5 @@ def contourSequence2Volume(cs, spacing=[1,1,1]):
     sten2img.SetOutsideValue(0)
     sten2img.SetInsideValue(255)
     sten2img.Update()
-    vtkimg=sten2img.GetOutput()
+    vtkimg = sten2img.GetOutput()
     return vtkimg
