066a8c27fba8bbfffa7c4c70f34354b5d04c9379
Dave Chen <dchen@mail.nih.gov>
2021-02-23 16:23:53 -0500

    checkin vtkio.py

---
+++ vtkio.py
@@ -0,0 +1,21 @@
+
+import vtk
+
+def writeVTKMesh(mesh, name):
+    """Write a VTK mesh file."""
+    try:
+        writer = vtk.vtkPolyDataWriter()
+        writer.SetInputData(mesh)
+        writer.SetFileTypeToBinary()
+        writer.SetFileName(name)
+        writer.Write()
+        print("Output mesh:", name)
+        writer = None
+    except BaseException:
+        print("VTK mesh writer failed")
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(
+            exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
+    return None
+
+
